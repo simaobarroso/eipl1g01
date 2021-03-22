91f28be5496a97c74058834d3f7272e46a5d179b
0801margarida <guidocapires@gmail.com>
2021-03-22 22:28:01 +0000

    main atualizada e a funcionar corretamente

--- code/Main.c
+++ code/Main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define CALC 100
+#define CALC 10240
 
 void stackop(Stack *,char *);
 
@@ -19,21 +19,28 @@ int main(void) {
         printf("%d", s.nums[i]);
     }
     putchar('\n');
+    free(s.nums);
     return 0;
 }
 
 void stackop(Stack *stack, char *calc) {
     int i = 0;
-
+   // strcat(calc, " ");
     while(calc[i] != '\n' && calc[i] != '\0') {
         // para nums
         char num[15] = "";
-        int sign = 1;
-            if(calc[i] >= '0' && calc[i] <= '9') { // 30 = '0' e 39 = '9'
-            // push((atoi(num)*sign), stack);
+        if(calc[i] >= '0' && calc[i] <= '9') { // 30 = '0' e 39 = '9'
+            int sign = 1;
+            
+            while(calc[i] >= '0' && calc[i] <= '9') {
+                if(i != 0 && calc[i-1] == '-') sign = -1;
+                strncat(num,&calc[i],1);
+                i++;
+            }
+            push((atoi(num)*sign), stack);
         }
         // para ops
-        else if(calc[i+1] == ' ' || calc[i+1] == '\0' || calc[i+1] == '\n') {
+        else if(calc[i] != ' ' && !(calc[i+1] >= '0' && calc[i+1] <= '9')){
             operation(calc[i],stack);
         }
         i++;
