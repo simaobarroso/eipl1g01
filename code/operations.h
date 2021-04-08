ace8f10b0802bf22d48ab1c172eb2693aa1df6d1
simaobarroso <simao.barroso@outlook.pt>
2021-04-09 00:02:14 +0100

    Movi a documentação. Ainda falta documentar a char

--- code/operations.h
+++ code/operations.h
@@ -1,20 +1,137 @@
 /**
  * @file Ficheiro com as várias invocações às funções de operações
  */
-#include <math.h>
 #include "stack.h"
 
-void operation(char,Stack *);
-int soma (int,int);
-int subtrai (int,int);
-int multiplica (int,int);
-int divide (int,int);
-int modulo (int,int);
-int potencia (int,int);
-int bitwiseand (int,int);
-int bitwiseor (int,int);
-int bitwisexor (int,int);
-int incrementa (int);
-int decrementa (int);
-int bitwisenot (int);
-char toChar (char);
+#include <math.h>
+
+/**
+ * \brief Os vários operadores matemáticos
+ */
+enum Operations {
+    Soma = '+',
+    Subtrai = '-',
+    Multiplica = '*',
+    Divide = '/',
+    Modulo = '%',
+    Potencia = '#',
+    Bitwiseand = '&',
+    Bitwiseor = '|',
+    Bitwisexor = '^',
+    Incrementa = ')',
+    Decrementa = '(',
+    Bitwisenot = '~',
+    ToChar = 'c',
+    Logico = 'e' // depois explico isto --Mota
+
+    // acrescentar os próximos operadores aqui (próximos guiões)
+};
+
+#define MATH_OPERATION_HEADER(func) Container func(Container, Container)
+
+/**
+ * \brief Realiza as operações a serem executadas
+ *
+ * @param Operador e Stack
+ *
+ */
+void operation(char, Stack*);
+/**
+ * \brief A soma de duas variáveis
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor da soma das variáveis
+ */
+MATH_OPERATION_HEADER(soma);
+/**
+ * \brief A subtração de duas variáveis
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor da subtração das variáveis
+ */
+MATH_OPERATION_HEADER(subtrai);
+/**
+ * \brief A multiplicação de duas variáveis
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor da multiplicação das variáveis
+ */
+MATH_OPERATION_HEADER(multiplica);
+/**
+ * \brief A divisão de duas variáveis
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor da divisão inteira das variáveis
+ */
+MATH_OPERATION_HEADER(divide);
+/**
+ * \brief O modulo de duas variáveis
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor do modulo das variáveis
+ */
+MATH_OPERATION_HEADER(modulo);
+/**
+ * \brief A potência de uma variável elevada à outra
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor da potência
+ */
+MATH_OPERATION_HEADER(potencia);
+/**
+ * \brief Forma um "e lógico" bitwise
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor do "e lógico"
+ */
+MATH_OPERATION_HEADER(bitwiseand);
+/**
+ * \brief Forma um bitwiseor
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor do bitwiseor
+ */
+MATH_OPERATION_HEADER(bitwiseor);
+/**
+ * \brief Forma um bitwisexor
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor do bitwisexor
+ */
+MATH_OPERATION_HEADER(bitwisexor);
+/**
+ * \brief Incrementação de uma unidade a uma variável int
+ *
+ * @param Uma variável do tipo int (integer)
+ *
+ * @returns O valor da variável acrescido de um
+ */
+Container incrementa(Container);
+/**
+ * \brief Decrementação de uma unidade a uma variável int
+ *
+ * @param Uma variável do tipo int (integer)
+ *
+ * @returns O valor da variável decrescido de um
+ */
+Container decrementa(Container);
+/**
+ * \brief Forma um bitwisenot
+ *
+ * @param Duas variáveis do tipo int (integer)
+ *
+ * @returns O valor do bitwisenot
+ */
+Container bitwisenot(Container);
+
+// TODO(Mota): é preciso documentar isto
+Container toChar(Container);
