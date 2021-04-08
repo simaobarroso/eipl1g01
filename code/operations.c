17c6c3f0a0f84ce762dad99887c8b934178c8e78
acmota2 <acmota2@gmail.com>
2021-04-08 23:59:07 +0100

    Alterei a forma como as operações são lidas nas funções de operações

--- code/operations.c
+++ code/operations.c
@@ -3,66 +3,38 @@
  */
 #include "operations.h"
 
-/**
- * \brief Os vários operadores matemáticos
- */
-enum Operations {
-    Soma = '+',
-	Subtrai = '-',
-	Multiplica = '*',
-	Divide = '/',
-	Modulo = '%',
-	Potencia = '#',
-	Bitwiseand = '&',
-	Bitwiseor = '|',
-	Bitwisexor = '^',
-	Incrementa = ')',
-	Decrementa = '(',
-	Bitwisenot = '~',
-    ToChar = 'c',           // eu já acrescentei isto --Mota
-    Logico = 'e'            // depois explico isto --Mota
-    
-    // acrescentar os próximos operadores aqui (próximos guiões)
-};
+#include <assert.h>
 
-/**
- * \brief Realiza as operações a serem executadas
- *
- * @param Operador e Stack  
- *
- */
-
-// isto está mesmo terrível, é preciso rever bué cenas aqui
-void operation(char op, Stack *stack) {
-    Container res;                  // isto está definitivamente mal --Mota
-    Container op1 = pop(stack);     // isto também --Mota
-    switch(op) {
+void operation(char op, Stack* stack) {
+    Container res;
+    Container op1 = pop(stack);
+    switch (op) {
         case Soma:
-            res = soma(pop(stack),op1);
+            res = soma(pop(stack), op1);
             break;
         case Subtrai:
-            res = subtrai(pop(stack),op1);
+            res = subtrai(pop(stack), op1);
             break;
         case Multiplica:
-            res = multiplica(pop(stack),op1);
+            res = multiplica(pop(stack), op1);
             break;
         case Divide:
-            res = divide(pop(stack),op1);
+            res = divide(pop(stack), op1);
             break;
         case Modulo:
-            res = modulo(pop(stack),op1);
+            res = modulo(pop(stack), op1);
             break;
         case Potencia:
-            res = potencia(pop(stack),op1);
+            res = potencia(pop(stack), op1);
             break;
         case Bitwiseand:
-            res = bitwiseand(pop(stack),op1);
+            res = bitwiseand(pop(stack), op1);
             break;
         case Bitwiseor:
-            res = bitwiseor(pop(stack),op1);
+            res = bitwiseor(pop(stack), op1);
             break;
         case Bitwisexor:
-            res = bitwisexor(pop(stack),op1);
+            res = bitwisexor(pop(stack), op1);
             break;
         case Incrementa:
             res = incrementa(op1);
@@ -74,151 +46,92 @@ void operation(char op, Stack *stack) {
             res = bitwisenot(op1);
             break;
         case ToChar:
-            res = toChar(stack);
+            res = toChar(op1);
+            break;
         default:
             return;
     }
-    push(res,stack);
+    push(res, stack);
 }
 
-/**
- * \brief A soma de duas variáveis
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor da soma das variáveis
- */
-int soma (int x, int y) {
-    return(x+y);
+#define MATH_OPERATION(func, op)                           \
+    Container func(Container x, Container y) {             \
+        Container res;                                     \
+        res.label = Double;                                \
+        if (x.label == Double && y.label == Double) {      \
+            res.content.f = op(x.content.f, y.content.f);  \
+        } else if (x.label == Double && y.label == Long) { \
+            res.content.f = op(x.content.f, y.content.l);  \
+        } else if (x.label == Long && y.label == Double) { \
+            res.content.f = op(x.content.l, y.content.f);  \
+        } else if (x.label == Long && y.label == Long) {   \
+            res.content.l = op(x.content.l, y.content.l);  \
+            res.label = Long;                              \
+        } else                                             \
+            assert(0 || "Error: wrong type");              \
+        return res;                                        \
+    }
+#define SOMA(x, y) x + y
+#define SUBTRAI(x, y) x - y
+#define MULTIPLICA(x, y) x* y
+#define DIVIDE(x, y) x / y
+#define INTEGER_OPERATION(func, op)                     \
+    Container func(Container x, Container y) {          \
+        Container res;                                  \
+        res.label = Long;                               \
+        if (x.label == Long && y.label == Long) {       \
+            res.content.l = x.content.l op y.content.l; \
+        } else                                          \
+            assert(0 || "Error: wrong type");           \
+        return res;                                     \
+    }
+
+MATH_OPERATION(soma, SOMA)
+MATH_OPERATION(subtrai, SUBTRAI)
+MATH_OPERATION(multiplica, MULTIPLICA)
+MATH_OPERATION(divide, DIVIDE)
+MATH_OPERATION(potencia, pow)
+INTEGER_OPERATION(bitwiseand, &)
+INTEGER_OPERATION(bitwiseor, |)
+INTEGER_OPERATION(bitwisexor, ^)
+
+Container incrementa(Container x) {
+    switch (x.label) {
+        case Double:
+            ++x.content.f;
+            break;
+        case Long:
+            ++x.content.l;
+            break;
+        case Char:
+            ++x.content.c;
+    }
+    return x;
 }
 
-/**
- * \brief A subtração de duas variáveis
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor da subtração das variáveis
- */
-int subtrai (int x, int y) {
-    return(x-y);
+Container decrementa(Container x) {
+    switch (x.label) {
+        case Double:
+            --x.content.f;
+            break;
+        case Long:
+            --x.content.l;
+            break;
+        case Char:
+            --x.content.c;
+    }
+    return x;
 }
 
-
-/**
- * \brief A multiplicação de duas variáveis
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor da multiplicação das variáveis
- */
-int multiplica (int x, int y) {
-    return(x*y);
+Container bitwisenot(Container x) {
+    if (x.label == Long) x.content.l = ~x.content.l;
+    return x;
 }
 
-/**
- * \brief A divisão de duas variáveis
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor da divisão inteira das variáveis
- */
-int divide (int x, int y) {
-    return((y != 0) 
-        ? (x / y)
-        : 0);
-} 
-
-
-/**
- * \brief O modulo de duas variáveis
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor do modulo das variáveis
- */
-int modulo (int x, int y) {
-    return(x % y);
-}
-
-/**
- * \brief A potência de uma variável elevada à outra
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor da potência
- */
-int potencia (int x, int y) {
-    return(pow (x, y));
-}
-
-/**
- * \brief Forma um "e lógico" bitwise
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor do "e lógico"
- */
-int bitwiseand (int x, int y) {
-    return(x & y);
-}
-
-/**
- * \brief Forma um bitwiseor
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor do bitwiseor
- */
-int bitwiseor (int x, int y) {
-    return(x | y);
-}
-
-
-/**
- * \brief Forma um bitwisexor
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor do bitwisexor
- */
-int bitwisexor (int x, int y) {
-    return(x ^ y);
-}
-
-/**
- * \brief Incrementação de uma unidade a uma variável int
- * 
- * @param Uma variável do tipo int (integer)
- *
- * @returns O valor da variável acrescido de um
- */
-int incrementa (int x) {
-	return ++x;
-}
-
-/**
- * \brief Decrementação de uma unidade a uma variável int
- * 
- * @param Uma variável do tipo int (integer)
- *
- * @returns O valor da variável decrescido de um
- */
-int decrementa (int x) {
-	return --x;
-}
-
-/**
- * \brief Forma um bitwisenot
- * 
- * @param Duas variáveis do tipo int (integer)
- *
- * @returns O valor do bitwisenot
- */
-int bitwisenot (int x) {
-    return ~x;
-}
-
-// acrescentei isto --Mota
-char toChar(char c) {
-    return c;
+Container toChar(Container x) {
+    if (x.label == Long) {
+        x.content.c = x.content.l;
+        x.label = Char;
+    }
+    return x;
 }
