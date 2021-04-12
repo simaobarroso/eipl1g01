63d8b44d71477f647e59f0d50b00539719d658f1
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2021-04-12 10:33:04 +0100

    documentacao da stack.c e stack.h

--- code/stack.c
+++ code/stack.c
@@ -13,43 +13,22 @@ void initializeContainer(Container* contains, Label l) {
     contains->content.l = 0;
 }
 
-/**
- * \brief Inicializa a Stack
- *
- * @param Stack
- */
+
 void initialize(Stack* list) {
     list->sizeofstack = 0;
     list->arr = malloc(SIZE * sizeof(Container));
     list->alloc = SIZE;
 }
 
-/**
- * \brief Verifica se a stack a stack está vazia
- *
- * @param Stack
- *
- * @returns Se está vazia retorna 1, caso contrário retorna 0
- */
+
 int isEmpty(Stack* list) {
     return !list->sizeofstack;
 }
-/**
- * \brief Verifica se uma stack está cheia
- *
- * @param Stack
- *
- * @returns Se está cheia retorna 1, caso contrário retorna 0
- */
+
 int isFull(Stack* list) {
     return (list->sizeofstack == list->alloc);
 }
 
-/**
- * \brief Coloca um valor no último índicie do array (topo da stack)
- *
- * @param Stack e um Int
- */
 void push(Container x, Stack* list) {
     if (isFull(list)) {
         list->alloc *= 2;
@@ -60,13 +39,6 @@ void push(Container x, Stack* list) {
     list->arr[list->sizeofstack++] = x;
 }
 
-/**
- * \brief Retira um valor no último índicie do array (topo da stack)
- *
- * @param Stack
- *
- * @returns Devolve o conteúdo desse índice
- */
 Container pop(Stack* list) {
     if (isEmpty(list)) {
         putchar('\n');
@@ -75,11 +47,6 @@ Container pop(Stack* list) {
     return list->arr[--list->sizeofstack];
 }
 
-/**
- * \brief Imprime a stack para o stdout.
- *
- * @param Stack
- */
 void printstack(Stack* stack) {
     for (int i = 0; i < stack->sizeofstack; i++) {
         if (stack->arr[i].label == Long)
--- code/stack.h
+++ code/stack.h
@@ -2,6 +2,10 @@
  * @file Ficheiro com as várias invocações às funções da Stack
  */
 /* expression.h */
+
+/**
+ * \brief Todos os tipos que vão ser usados na Stack
+ */
 typedef enum { // ir acrescentando nos próximos guiões
     Long,
     Double,
@@ -10,6 +14,9 @@ typedef enum { // ir acrescentando nos próximos guiões
     NotAType
 } Label;
 
+/**
+ * \brief Estrutura de um container, que controla o tipo dos elementos da stack
+ */
 typedef struct { // idém ^^
     Label label;
     union {
@@ -20,15 +27,61 @@ typedef struct { // idém ^^
     } content;
 } Container;
 
+/**
+ * \brief Controla os elementos da stack
+ */
 typedef struct {
     Container* arr;  // stack itself
-    int sizeofstack; // tamanho da stack
+    int sizeofstack; // número de elemntos da stack
     int alloc;       // número alocado
 } Stack;
 
+/**
+ * \brief Inicializa a Stack
+ *
+ * @param Stack
+ */
 void initialize(Stack*);
+/**
+ * \brief Inicializa um conteiner com a label indicada
+ *
+ * @param Container e uma label
+ */
 void initializeContainer(Container*,Label);
-void changecurrent(Stack*, int);
+/**
+ * \brief Verifica se a stack a stack está vazia
+ *
+ * @param Stack
+ *
+ * @returns Se está vazia retorna 1, caso contrário retorna 0
+ */
+int isEmpty(Stack* list);
+/**
+ * \brief Verifica se uma stack está cheia
+ *
+ * @param Stack
+ *
+ * @returns Se está cheia retorna 1, caso contrário retorna 0
+ */
+int isFull(Stack* list)
+
+/**
+ * \brief Coloca um valor no último índicie do array (topo da stack)
+ *
+ * @param Stack e um Int
+ */
 void push(Container, Stack*);
+/**
+ * \brief Retira um valor no último índicie do array (topo da stack)
+ *
+ * @param Stack
+ *
+ * @returns Devolve o conteúdo desse índice
+ */
 Container pop(Stack*);
+/**
+ * \brief Imprime a stack para o stdout.
+ *
+ * @param Stack
+ */
 void printstack(Stack*);
