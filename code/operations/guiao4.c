ac3ec4cd6551d38b10fad270fedd6b085b41ef99
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2021-04-26 23:39:17 +0100

    inicio das funcoes do guiao4

--- code/operations/guiao4.c
+++ code/operations/guiao4.c
@@ -7,3 +7,61 @@
 #include "../parser/parser.h"
 #include "operations.h"
 
+
+/**
+ * \brief Coloca um array numa stack
+ *
+ * @param Stack 
+ * @param Array 
+ *
+ */
+void ColocarStack(Stack *stack, Container *array){
+for(int n= 0;n < array.ARRAY->sizeofstack;n++) push(array.ARRAY->arr[n], stack);
+}
+
+void ConcatenarSA(){
+
+}
+
+void TamnhoRange(){
+
+}
+
+/**
+ * \brief Mesma função definida em cima
+ *
+ * É colocada aqui por preferência, de modo a tornar o código mais legível
+ *
+ * @param Array 
+ * @param Int (indice para o array dado) 
+ *
+ */
+Container Indice(Container array, Container indice) {
+    return array.ARRAY[indice.LONG];
+}
+
+void BuscarX(){
+
+}
+
+void Remover(){
+
+}
+
+void Substring(){
+
+}
+
+
+void SepararSub(){
+
+}
+
+void SepararSpace(){
+
+}
+
+void SepararLines(){
+
+}
+
