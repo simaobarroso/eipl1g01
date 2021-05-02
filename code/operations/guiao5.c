903bfd775fd6ebed3a25ef4db32f98d6ce6976c1
Simão Barroso <73468752+simaobarroso@users.noreply.github.com>
2021-05-02 22:10:50 +0100

    funçoes a fazer guiao5

--- code/operations/guiao5.c
+++ code/operations/guiao5.c
@@ -4,6 +4,38 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include "operations.h"
+#include "../stack.h"
+#include "../parser/parser.h"
+
+#include <assert.h>
+#include <stdlib.h>
+/*
+void parser_block() {
+    MOTA POR FAVOR TRATA DISTO :(
+}
+*/
+
+void criarbloco(){
+
+
+}
+
+void aplicarbloco(){
+
+}
+
+void fold () {
+
+}
+
+void filter(){
+
+}
+
+void ordenar(){
+
+}
 
 void executar_bloco(Stack s) {
 }
