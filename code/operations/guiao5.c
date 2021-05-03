51ff7dbdb18646785d21da3c4b75f7f57dba2c0f
André Mota <acmota2@gmail.com>
2021-05-03 10:12:00 +0100

    vamos ver se resulta...

--- code/operations/guiao5.c
+++ code/operations/guiao5.c
@@ -16,26 +16,52 @@ void parser_block() {
 }
 */
 
-void criarbloco(){
-
-
-}
-
 void aplicarbloco(){
 
 }
 
-void fold () {
-
+void map(Stack s, Container x, Container y, OperatorFunction* hashtable, Container* vars) {
+    Stack map = initialize_stack();
+    if (x.label == String) string_to_array(x);
+    Label res_label = x.label;
+    int i = 0;
+    while (x.ARRAY->sizeofstack) {
+        push(x.ARRAY->arr[i],map);
+        parser(map,y.STRING,hashtable,vars);
+        x.ARRAY->sizeofstack--;
+        i++;
+    }
+    free(x.ARRAY);
+    Container res = { .label = res_label, .ARRAY = map };
+    push(res,s);
 }
 
-void filter(){
-
+void fold(Stack s, Container x, Container y, OperatorFunction* hashtable, Container* vars) {
+    while (x.ARRAY->sizeofstack != 1) {
+        parser(x.ARRAY,y.STRING,hashtable,vars);
+    }
+    Container res = pop(x.ARRAY);
+    push(res,s);
 }
 
-void ordenar(){
-
+void filter(Stack s, Container x, Container y, OperatorFunction* hashtable, Container* vars) {
+    Stack filter = initialize_stack();
+    if (x.label == String) string_to_array(x);
+    Label res_label = x.label;
+    int i = 0;
+    while (x.ARRAY->sizeofstack) {
+        push(x.ARRAY->arr[i],filter);
+        parser(x.ARRAY,y.STRING,hashtable,vars);
+        Container check = toDouble(filter->arr[filter->sizeofstack - 1]);
+        if (check.DOUBLE == 0) pop(filter);
+        x.ARRAY->sizeofstack--;
+        i++;
+    }
+    free(x.ARRAY);
+    Container res = { .label = res_label, .ARRAY = filter };
+    push(res,s);
 }
 
-void executar_bloco(Stack s) {
+void ordenar() {
+
 }
